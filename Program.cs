b9210f1033e4fc497610b73d22cb4ae0615c3b64
Christian Perez <cristianperez028@gmail.com>
2025-06-03 21:07:39 -0500

    Added middlewares

--- Program.cs
+++ Program.cs
@@ -8,6 +8,27 @@ builder.Services.AddTransient<ILoginService, LoginService>();
 
 var app = builder.Build();
 
+app.Use( async(context, next) =>
+{
+    var loginService = context.RequestServices.GetService<ILoginService>();
+    loginService.Authenticate("test", "testPassword");
+    await next();
+});
+
+app.Use( async(context, next) =>
+{
+    var loginService = context.RequestServices.GetService<ILoginService>();
+    loginService.Authenticate("user2", "testPassword2");
+    await next();
+});
+
+app.Use( async(context, next) =>
+{
+    var loginService = context.RequestServices.GetService<ILoginService>();
+    loginService.Authenticate("user3", "testPassword3");
+    await next();
+});
+
 
 
 
